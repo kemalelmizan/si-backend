e6b4ae01e88dabad456d67359234c4a0e8874f54
kemalelmizan <kemalelmizan@gmail.com>
2018-11-25 17:13:38 +0700

    add * in headers

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(function(req, res, next) {
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "*"
   );
   if (req.method === "OPTIONS") {
     return res.status(200).end();
