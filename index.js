3258b1889323ac7839808f2d0f5658c40f49d8b2
kemalelmizan <kemalelmizan@gmail.com>
2018-11-25 17:11:51 +0700

    add options return ok

--- index.js
+++ index.js
@@ -28,8 +28,17 @@ app.use("/robots.txt", (req, res) => {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,PATCH,DELETE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
   next();
 });
 
